28a2987bd51f2867a4162407efca543e69c1948f
Md. Rakibul Islam <rakibul00206@gmail.com>
2026-01-17 19:21:26 +0600

    get all products endpoint completed

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -2,6 +2,39 @@ import connectDB from "@/lib/db";
 import Product from "@/models/Product";
 import { NextResponse } from "next/server";
 
+export const GET = async (req) => {
+  try {
+    const sort = {};
+    const searchParams = req.nextUrl.searchParams;
+    const limit = Number(searchParams.get("limit")) || 10;
+    const skip = Number(searchParams.get("skip")) || 0;
+    const sortBy = searchParams.get("sortBy")?.trim() || "createdAt";
+    const sortOrder = searchParams.get("sortOrder")?.trim() === "asc" ? 1 : -1;
+
+    if (sortBy && sortOrder) {
+      sort[sortBy] = sortOrder;
+    }
+
+    await connectDB();
+
+    const products = await Product.find().sort(sort).limit(limit).skip(skip);
+
+    return NextResponse.json({
+      success: true,
+      message: "Products retrieved successfully",
+      total: products.length,
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      { success: false, message: "Failed to retrieve products" },
+      { status: 500 },
+    );
+  }
+};
+
 export const POST = async (req) => {
   try {
     await connectDB();
@@ -40,11 +73,9 @@ export const POST = async (req) => {
       { status: 201 },
     );
   } catch (error) {
-    console.error("Error creating product:", error);
-
     return NextResponse.json(
-      { success: false, message: error.message || "Failed to create product" },
-      { status: error.status || 500 },
+      { success: false, message: "Failed to create product" },
+      { status: 500 },
     );
   }
 };
